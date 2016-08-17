67a1303bf97213ebef56c9cef0ae68a603350ce3
Ian Lai <ian@fyianlai.com>
2016-08-17 22:26:16 +0100

    Fix `media` plugin / hack for style elements
    The `media` attribute is automatically removed for stylesheet
    links if there are no print styles defined.
    
    Previously however, the aforementioned 'autoremoving' behaviour
    only applied to elements with `rel=stylesheet`, and it did not
    apply to `style` elements.
    
    This commit addresses the problem by including `style` elements
    in the query selector.
    
    ---
    
    Fixes #61 (Github)
    
    Signed-off-by: Ian Lai <ian@fyianlai.com>

--- cli/src/plugin_media.js
+++ cli/src/plugin_media.js
@@ -1,5 +1,5 @@
-var print = document.querySelectorAll('[rel="stylesheet"][media*="print"]');
-var screen = document.querySelectorAll('[rel="stylesheet"][media*="screen"]');
+var print = document.querySelectorAll("[rel='stylesheet'][media*='print'], style[media*='print']");
+var screen = document.querySelectorAll("[rel='stylesheet'][media*='screen'], style[media*='screen']");
 
 if (print.length === 0) {
     for (var i = 0, l = screen.length; i < l; i++) {
