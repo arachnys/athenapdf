e65f567fd118120ea508eaa3e7fab496deedbae2
Ian Lai <ian@fyianlai.com>
2017-07-28 11:23:22 +0100

    [Version 3] Clean up Docker images, and create build script
    This is an attempt at reducing duplication across the Docker images.
    Now, the Go build command can be modified in a single source.
    
    Also, add usage instructions based on Label Schema specs:
    http://label-schema.org/rc1/

---
+++ bin/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Builds athenapdf go assemblies
+# Arachnys <techteam@arachnys.com>
+# https://github.com/arachnys/athenapdf/
+#
+
+set -ex
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Build output or package not specified"
+    exit 1
+fi
+
+CGO_ENABLED=0 \
+    go build \
+    -ldflags "-s" -a -installsuffix cgo \
+    -o "$1" \
+    "$2"
